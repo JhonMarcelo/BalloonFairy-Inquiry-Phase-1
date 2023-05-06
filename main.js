e5e449d053d9bcb70fabaf5f5003826328f4f164
Jhon Waree Marcelo <jhon.waree.marcelo@gmail.com>
2023-05-06 15:20:05 -0400

    adds div on each ballon type and added ID on each of their divs

--- main.js
+++ main.js
@@ -4,3 +4,17 @@ console.log("Test")
 fetch("http://localhost:3000/client_Inquiry")
 .then(res=>res.json())
 .then(client => console.log(client))
+
+
+document.addEventListener("DOMContentLoaded",()=>{
+    fetch("http://localhost:3000/balloon_types")
+    .then(res => res.json())
+    .then(balloon => balloon.forEach(balloonPic => createBalloonGalrey(balloonPic)))
+        
+});
+
+//FUNCTIONS
+
+function createBalloonGalrey(balloon){
+
+}
