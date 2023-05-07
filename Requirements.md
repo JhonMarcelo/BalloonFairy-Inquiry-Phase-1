2134f71717245d0f360a68e794a3e90a689b8460
Jhon Waree Marcelo <jhon.waree.marcelo@gmail.com>
2023-05-07 09:47:54 -0400

    adds mouseover eventListener to image1 and test it with console.log

---
+++ Requirements.md
@@ -0,0 +1,5 @@
+Y-HTML/CSS/JS
+Y-atleast 5 objects with 3 attributes each.
+Y-SINGLE HTML file ONLY
+X-use 2 distinct eventllistener
+Y-must use atleast one array itteration
--- main.js
+++ main.js
@@ -24,11 +24,20 @@ function createBalloonGalrey(balloon){
 
     for(let i =0; i < balloonType.length; i++){
         if(balloonType[i].id === balloon.type){
+            let description = document.createElement('p')
             let img1 = document.createElement("img")
+            img1.addEventListener("mouseover",()=>{
+                console.log("hovered")
+            } )
+            
+            //add description from db.json to p
+            description.textContent = balloon.description
+
+            //assign the img URL from db.json to an img element in html
             img1.classList.add(`${balloon.type}img1`)
             img1.src = balloon.img1
-            img1.width = "300"
-            img1.height = "200"
+            img1.width = "400"
+            img1.height = "300"
 
 
 //*******FIX SIZING FIRST BEFORE PUTTING THIS 2 IMG BACK */
@@ -36,18 +45,18 @@ function createBalloonGalrey(balloon){
             let img2 = document.createElement("img")
             img2.classList.add(`${balloon.type}img2`)
             img2.src = balloon.img2
-            img2.width = "300"
-            img2.height = "200"
+            img2.width = "400"
+            img2.height = "300"
 
             let img3 = document.createElement("img")
             img3.classList.add(`${balloon.type}img3`)
             img3.src = balloon.img3
-            img3.width = "300"
-            img3.height = "200"
+            img3.width = "400"
+            img3.height = "300"
 
             
             //,img2,img3 down there
-            balloonType[i].append(img1,img2,img3)
+            balloonType[i].append(description,img1,img2,img3)
         }
 
     }
